357435859490fb1f019994e0f2b799c8a4b084ba
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-01-19 15:42:04 +0300

    Data Binding and Boxing

---
+++ CoursesApp/Helpers/Box.swift
@@ -0,0 +1,29 @@
+//
+//  Box.swift
+//  CoursesApp
+//
+//  Created by Nikita Chekan on 19.01.2023.
+//
+
+import Foundation
+
+class Box<T> {
+    typealias Listener = (T) -> Void
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    private var listener: Listener?
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(listener: @escaping Listener) {
+        self.listener = listener
+        listener(value)
+    }
+}
